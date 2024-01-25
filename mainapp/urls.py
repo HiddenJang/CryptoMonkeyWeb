307721a1824831088ef3c52439d8c1e68a09749e
HiddenJang <jangljungl@gmail.com>
2024-01-25 22:06:13 +0400

    some changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.login_page, name='login_page'),
@@ -13,3 +15,5 @@ urlpatterns = [
     path('app_mainpage', views.app_mainpage, name='app_mainpage'),
     path('app_mainpage/', views.app_mainpage, name='app_mainpage'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
